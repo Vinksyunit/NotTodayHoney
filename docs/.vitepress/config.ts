4f81f217d7bc1c438863db582b3450f5d7d182d7
Vincent Demonchy <vinksyunit@gmail.com>
2026-03-29 17:37:53 +0200

    chore: configure VitePress with orange theme

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: 'NotTodayHoney',
+  description: 'Laravel honeypot package to detect and block attackers',
+  srcExclude: ['superpowers/**'],
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'GitHub', link: 'https://github.com/Vinksyunit/NotTodayHoney' },
+    ],
+    sidebar: [
+      { text: 'Getting Started', link: '/getting-started' },
+      { text: 'Configuration', link: '/configuration' },
+      { text: 'Traps', link: '/traps' },
+      { text: 'Events & Middleware', link: '/events-middleware' },
+      { text: 'Artisan Commands', link: '/commands' },
+    ],
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/Vinksyunit/NotTodayHoney' },
+    ],
+  },
+})
