ff261dff8cf66e4c1fc1bd4a874697a7f0917ed6
Vincent Demonchy <vinksyunit@gmail.com>
2026-03-29 11:43:05 +0200

    fix: add RefreshDatabase to Feature tests in Pest.php

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Vinksyunit\NotTodayHoney\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
+uses(RefreshDatabase::class)->in('Feature');
