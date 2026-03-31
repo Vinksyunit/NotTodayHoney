2633506066b9deda83dca898fd6791a4c785e2e9
Vincent Demonchy <vinksyunit@gmail.com>
2026-03-31 11:51:12 +0200

    docs(nav): restructure sidebar into Guides and Components groups

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -13,10 +13,24 @@ export default defineConfig({
     ],
     sidebar: [
       { text: 'Getting Started', link: '/getting-started' },
-      { text: 'Configuration', link: '/configuration' },
-      { text: 'Traps', link: '/traps' },
-      { text: 'Events & Middleware', link: '/events-middleware' },
-      { text: 'Artisan Commands', link: '/commands' },
+      { text: 'Blue Team Practices', link: '/blue-team' },
+      {
+        text: 'Guides',
+        items: [
+          { text: 'Detect & Get Notified', link: '/guides/detect-and-notify' },
+          { text: 'Protect Your Features', link: '/guides/protect-features' },
+          { text: 'Compromised Passwords', link: '/guides/passwords' },
+        ],
+      },
+      {
+        text: 'Components',
+        items: [
+          { text: 'Traps', link: '/traps' },
+          { text: 'Configuration', link: '/configuration' },
+          { text: 'Events & Middleware', link: '/events-middleware' },
+          { text: 'Artisan Commands', link: '/commands' },
+        ],
+      },
     ],
     socialLinks: [
       { icon: 'github', link: 'https://github.com/Vinksyunit/NotTodayHoney' },
